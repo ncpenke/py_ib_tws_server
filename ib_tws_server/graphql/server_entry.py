e3a460a3ba2f2da26c1547834e111c90fa2a1875
npenke <16369152+ncpenke@users.noreply.github.com>
2021-09-27 16:29:20 -0700

    Implement ASGI lifetime methods for clean shutdown

--- ib_tws_server/graphql/server_entry.py
+++ ib_tws_server/graphql/server_entry.py
@@ -1,49 +1,69 @@
-from ariadne import MutationType, QueryType, SubscriptionType, make_executable_schema, load_schema_from_path, convert_kwargs_to_snake_case
+from ariadne import make_executable_schema, load_schema_from_path
 from ariadne.asgi import GraphQL
-from ariadne.graphql import subscribe
-from ariadne.objects import MutationType
-import asyncio
 from ib_tws_server.gen.asyncio_client import AsyncioClient
 from ib_tws_server.gen.graphql_resolver import query, subscription, graphql_resolver_set_client, union_types
 import logging
 import os
 from starlette.middleware.cors import CORSMiddleware
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
 import sys
+from typing import Union
 
 logging.basicConfig(stream=sys.stdout, level=logging.WARN)
 logger = logging.getLogger()
 
+class IbClientLifespan:
+    app: ASGIApp
+    receive: Receive
+    send: Send
+    ib_client: AsyncioClient
+
+    def __init__(self, app: ASGIApp):
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope["type"] != "lifespan":
+            await self.app(scope, receive, send)
+            return
+
+        msg = await receive()
+        if msg["type"] != "lifespan.startup":
+            raise RuntimeError(f"Unexpected Lifetime event {msg}")
+        print("REceived start up...")
+        self.ib_client = self.create_ib_client()
+        await send({"type": "lifespan.startup.complete"})
+        msg = await receive()
+        if msg["type"] != "lifespan.shutdown":
+            raise RuntimeError(f"Unexpected Lifetime event {msg}")
+        self.ib_client.disconnect(True)
+        await send({"type": "lifespan.shutdown.complete"})
+
+    def create_ib_client(self):
+        c = AsyncioClient()
+        host = os.getenv('IB_SERVER_HOST')
+        port = os.getenv('IB_SERVER_PORT')
+
+        if host is None:
+            logger.warn(f"IB_SERVER_HOST is not set. Using default")
+            host = "127.0.0.1"
+        if port is None:
+            logger.warn(f"IB_SERVER_PORT is not set. Using default")
+            port = 7496
+        else:
+            port = int(port)
+
+        connection_retry_interval = 5
+        c.start(host, port, 0, connection_retry_interval)
+        graphql_resolver_set_client(c)
+        return c
+
 def create_app():
     type_defs = load_schema_from_path("ib_tws_server/gen/schema.graphql")
-    mutation = MutationType()
-    queue = asyncio.Queue(maxsize=0)
-
-    users=dict()
-    messages=[]
-    queues = []
-
     schema = make_executable_schema(type_defs, query, subscription, union_types)
-    return CORSMiddleware(app=GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
-
-def create_ib_client():
-    c = AsyncioClient()
-    host = os.getenv('IB_SERVER_HOST')
-    port = os.getenv('IB_SERVER_PORT')
-
-    if host is None:
-        logger.warn(f"IB_SERVER_HOST is not set. Using default")
-        host = "127.0.0.1"
-    if port is None:
-        logger.warn(f"IB_SERVER_PORT is not set. Using default")
-        port = 7496
-    else:
-        port = int(port)
-
-    connection_retry_interval = 5
-    c.start(host, port, 0, connection_retry_interval)
-    graphql_resolver_set_client(c)
-    return c
+    ariadneApp = GraphQL(schema, debug=True)
+    # Wrap ariadne with CORS middleware for CORS handling
+    corsWrapper = CORSMiddleware(app=ariadneApp, allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
+    # Wrap with IB client lifespan wrapper
+    return IbClientLifespan(corsWrapper)
 
 app = create_app()
-ib_client = create_ib_client()
-
